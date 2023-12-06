0dd79d46b4aadced2c1f3dba9c73966a669d26a0
yogaardiansyah <yogaardiansyah@protonmail.com>
2023-12-06 17:46:03 +0700

    Ubah sedikit

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
-module Beego
+module Bego
 
 go 1.20
 
-require github.com/go-sql-driver/mysql v1.7.1 // indirect
+require github.com/go-sql-driver/mysql v1.7.1
--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"Beego/config"
-	"Beego/controllers/control"
+	"Bego/config"
+	"Bego/controllers/control"
 	"log"
+	"net"
 	"net/http"
+	"strings"
 )
 
 func main() {
@@ -16,7 +18,40 @@ func main() {
 	control.Router("/testing", "home/index.html", "GET")
 
 	// Server Run
-	log.Println("Server running on port 8080")
-	http.ListenAndServe(":8080", nil)
+	server := &http.Server{Addr: ":8080"}
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+	}()
 
+	// Get the local address after the server starts
+	addr := server.Addr
+	if addr == "" {
+		addr = ":http"
+	}
+
+	// Get the IP address of the server
+	ip, err := getLocalIP()
+	if err == nil {
+		addr = ip + addr[strings.LastIndex(addr, ":"):]
+	}
+
+	log.Printf("Server running on http://%s", addr)
+
+	// Allow the server to keep running until manually stopped (Ctrl+C)
+	select {}
+}
+
+func getLocalIP() (string, error) {
+	conn, err := net.Dial("udp", "8.8.8.8:80") // Google's public DNS IP
+	if err != nil {
+		return "", err
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddr.IP.String(), nil
 }
